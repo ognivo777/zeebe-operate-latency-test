a1a55b7b810e805281a0f71c427cd914cc40c439
ognivo777 <ognivo777@gmail.com>
2024-10-26 18:58:21 +0300

    Beta version

--- src/main/java/ru/obiz/zeebe/play/Main.java
+++ src/main/java/ru/obiz/zeebe/play/Main.java
@@ -15,8 +15,6 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         Properties properties = new Properties();
         properties.put("zeebe.client.broker.gateway-address", "127.0.0.1:26500");
         properties.put("zeebe.client.security.plaintext", "true");
